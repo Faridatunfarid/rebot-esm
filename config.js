613bcf1b083fd934deeb3042cbf860986e8c4099
Mas F <94673633+Fjr-api@users.noreply.github.com>
2026-03-16 13:27:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,30 +4,35 @@
 * Follow https://github.com/Fjr-api
 */
 
-  global.owner = ["105974090227874","6289692509996","6289699872867"] // nomor owner
+
+  global.owner = ["105974090227874","221036851261537","6289692509996","6281234567"] // nomor owner (bisa pake lid or number) recomended number
   global.ownerJid = '6289692509996'
   global.prefix = "." // prefix command
-  global.botName = "Rebot ESM"
+  global.botName = "Rebot"
   global.footer = "*Rebot™© | 2019-2025*"
-  global.pairingNumber = "6289699872867"
-  
+  global.pairingNumber = "6281234567" // WAJIB ISI NO BOT KALIAN UNTUK PAIRING!!
+  global.packname = "(6281234567)";
+  global.author = "Sticker.by Rebot";
+
+  global.apikeyGemini = "-"; //isi apikey gemini AI kalian
+
   global.limitawal = {
     moderator: 5000,
     premium: 'Infinity',
     free: 15,
   };
+
   global.mess = {
     success: "✓ Success",
     admin: "_ⓘ Fitur Khusus Admin Group!_",
     botAdmin: "_ⓘ Bot Harus Menjadi Admin Terlebih Dahulu!_",
     owner: "_ⓘ Fitur Khusus Owner Bot_",
-    mod: "_ⓘ Fitur ini hanya bisa digunakan oleh owner atau mod_",
+    mod: "_ⓘ Fitur ini hanya bisa digunakan oleh owner atau mod_\n\n> _-Ketik .toko untuk membeli hak MOD_",
     group: "_ⓘ Fitur Digunakan Hanya Untuk Group!_",
     private: "_ⓘ Fitur Digunakan Hanya Untuk Private Chat!_",
     bot: "_ⓘ Fitur Khusus Pengguna Nomor Bot_",
     wait: "Loading...",
-    endLimit: "_ⓘ Limit Harian kamu Telah Habis, Limit Akan Direset Setiap Jam 00.00 WITA, atau kamu bisa membeli limit denngan ketik .buy_",
+    endLimit: "_ⓘ Limit Harian kamu Telah Habis, Limit Akan Direset Setiap Jam 00.00 WITA, atau kamu bisa membeli limit denngan ketik .toko_",
     spam: "_ⓘ Maaf kamu masih dalam status banned sementara karena sebelumnya melakukan spam, gunakan fitur *.cekspam* untuk melihat sisa coldown_",
-    err: "> _ⓘ Sepertinya terjadi kesalahan pada fitur yang kamu gunakan. Pastikan untuk memeriksa kembali cara penggunaan fitur tersebut._\n> _Jika masalah terus berlanjut, silakan laporkan ke owner bot menggunakan fitur_ *.report* atau *.lapor.*"
-
+    err: "> _ⓘ Sepertinya terjadi kesalahan pada fitur yang kamu gunakan. Pastikan untuk memeriksa kembali cara penggunaan fitur tersebut._\n\n> _Jika masalah terus berlanjut, silakan laporkan ke owner bot menggunakan fitur_ *.report* atau *.lapor.*"
   };
